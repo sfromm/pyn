579306dd513ff8358b65d39097c970aa70cce252
Stephen Fromm <sfromm@gmail.com>
2014-09-08 15:28:48 -0700

    cleanup

--- 1-1.py
+++ 1-1.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 
 from snmp_helper import snmp_extract, snmp_get_oid
-from pysnmp.entity.rfc3413.oneliner import cmdgen
 from optparse import OptionParser
 import sys
 
+# HARD CODE IT!  :P
 OIDS = { 'sysName' : '1.3.6.1.2.1.1.5.0' ,
          'sysDescr' : '1.3.6.1.2.1.1.1.0' }
 
@@ -17,7 +17,7 @@ def main(args):
         host, port = arg.split(':')
         device = (host, options.community, port)
         for name, oid in OIDS.iteritems():
-            print snmp_extract(snmp_get_oid(device, oid=get_oid(oid)))
+            print snmp_extract(snmp_get_oid(device, oid=oid))
 
 if __name__ == '__main__':
     try:
